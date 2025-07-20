55db6d7fc44abf76a53f8c7eb49abe7c840c7b50
cindxyc <chencindy.1206@gmail.com>
2025-07-20 00:35:22 -0400

    EXPERIENCE SECTION DONEEEEEgit add .git add . '
    FINALLY ADDED EXPERIENCE!!!!!!!

--- src/data.js
+++ src/data.js
@@ -15,33 +15,61 @@ const social = [
 const brand = 'XYC'
 
 const experience = [
+	// Incoming Experience Section
+	{
+		position: 'Software Developer Intern',
+		company: 'Pratt & Whitney Canada',
+		start: 'September 2025',
+		end: 'TBD',
+		description: 'Upcoming position starting in fall 2025.',
+		logo: '/src/assets/p&w_logo.jpeg',
+		link: 'https://www.prattwhitney.com/en',
+		incoming: true
+  	},
 	{
 		position: 'Frontend Developer',
 		company: 'UofTHacks XIII',
 		start: 'May 2025',
 		end: 'Present',
-		description: 'Implement the teaser and home page for Canadian\'s first student run hackathon.'
+		description: 'Implement the teaser and home page for Canadian\'s first student run hackathon.',
+		logo: '/src/assets/uofthacks_logo.jpeg',
+		link: 'https://uofthacks.com/'
+	},
+	{
+		position: 'IBM Z Student Ambassador',
+		company: 'IBM Z',
+		start: 'February 2024',
+		end: 'December 2024',
+		description: 'Completed the IBM Zxplore Concepts badge.\nGained mainframe technology knowledge and developed essential leadership competencies.',
+		logo: '/src/assets/ibm_logo.jpeg',
+		link: 'https://www.ibm.com/products/z/resources/zxplore'
 	},
 	{
 		position: 'Full Stack Developer',
 		company: 'Inwit',
 		start: 'May 2024',
 		end: 'August 2024',
-		description: 'Revamped the the UI for an e-commerce food catering platform (inwit.app) using PHP and Tailwind CSS. Added a client portal with an order management dashboard featuring order history tracking, stored using MySQL database.'
+		description: 'Revamped the UI for inwit.app (e-commerce food catering platform). \n Added a client portal with an order management dashboard.',
+		logo: '/src/assets/inwit_logo.jpeg',
+		link: 'https://www.inwit.app'
 	},
 	{
 		position: 'Software Developer Intern',
 		company: 'Cove Neuroscience',
 		start: 'February 2024',
 		end: 'May 2024',
-		description: 'Implemented functions to the pipeline to automatically generate detailed PDF reports from analyzed brain data.'
+		description: 'Implemented functions to the pipeline to automatically generate detailed PDF reports from analyzed brain data.',
+		logo: '/src/assets/cove_logo.png',
+		link: 'https://www.coveneuro.com'
 	},
 	{
 		position: 'Web Developer & Marketing',
 		company: 'Woodsworth College @ University of Toronto',
 		start: 'May 2022',
 		end: 'September 2023',
-		description: 'Developed and maintained the Orientation website and archived $20k in gross revenue.'
+		description: 'Developed and maintained the Orientation website and archived $20k in gross revenue.',
+		logo: '/src/assets/woodsworth_logo.jpeg',
+		link: 'https://www.instagram.com/wworientation2025/'
 	}
 ]
 
