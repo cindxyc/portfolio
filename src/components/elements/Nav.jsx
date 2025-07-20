4ba553af31c6dcf66843e70d5df2771e411e2922
cindxyc <chencindy.1206@gmail.com>
2025-07-20 03:44:14 -0400

    RESUME WORKS NOW

--- src/components/elements/Nav.jsx
+++ src/components/elements/Nav.jsx
@@ -29,14 +29,17 @@ const Nav = () => {
 					<a className='Nav-link' href='#experience' onClick={() => setMenu(false)}>
 						Experiences.
 					</a>
-					<a
-						className="Nav-link"
-						href="/Hubert_Xu_Resume.pdf"
-						target="_blank"
-						rel="noopener noreferrer"
-						onClick={() => setMenu(false)}
-					>
-						Résumé
+					<a className='Nav-link' href='#projects' onClick={() => setMenu(false)}>
+						Projects.
+					</a>
+						<a
+							className="Nav-link"
+							href="/Cindee_Resume.pdf"
+							target="_blank"
+							rel="noopener noreferrer"
+							onClick={() => setMenu(false)}
+						>
+						Resume.
 					</a>
 						<a
 						className='Nav-link'
